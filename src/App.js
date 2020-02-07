92ce9c28a6222b36a45c8094ea23ad7bcd537dbd
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-02-07 14:13:51 +0200

    apollo-client graphQL practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost'
+
+const Countries = ({ GET_COUNTRIES }) => {
+  const { loading, error, data } = useQuery(GET_COUNTRIES)
+
+  if(loading) return <h3>Loading...</h3>
+  if(error) return `Error ${error.message}`
+
+  const countries = data.countries.map((country, index) => {
+    return (
+      <tr key={index}>
+        <td>{country.code}</td>
+        <td>{country.name}</td>
+        <td>{country.continent.name}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Code</th>
+            <th>Country</th>
+            <th>Continent</th>
+          </tr>
+          {countries}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 function App() {
+  const client = new ApolloClient ({
+    uri:'https://countries.trevorblades.com/',
+  })
+
+  const GET_COUNTRIES = gql`
+    {
+      countries {
+        code,
+        name,
+        continent {
+          name,
+        }
+      }
+    }
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <ApolloProvider client={client}>
+     <div>
+       <Countries GET_COUNTRIES={GET_COUNTRIES}/>
+     </div>
+   </ApolloProvider>
+  )
 }
 
 export default App;
